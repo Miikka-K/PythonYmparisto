afeed662a8f09f0d34c02965a4ea4fb5cd628b8e
Miikka-K <51471@edu.raseko.fi>
2024-09-12 15:07:57 +0300

    Lisätty winsound kirjasto ja piippaa funktio

--- app.py
+++ app.py
@@ -7,7 +7,21 @@
 # OpenCV-kirjasto videokameraa varten
 import cv2
 
+# Winsound-kirjasto äänikorttia varten
+import winsound
+
 # ASETUKSET
 # ---------
 
 kameraIndeksi = 0 # Ensimmäinen kamera on aina 0
+
+# Funktiot
+# --------
+
+def piippaa():
+    """Tuottaa puolen sekunnin 1kHz äänimerkin"""
+    taajuus = 1000   # Hz
+    kesto   = 500    # ms
+    winsound.Beep(taajuus, kesto)
+
+piippaa()
